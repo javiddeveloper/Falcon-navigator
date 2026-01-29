e236c9794c1aa062133031b9255b443617b00aaa
javid <javiddeveloper@gmail,com>
2026-01-29 19:47:17 +0330

    set status bar to dark mode

--- app/src/main/java/xyz/javidsattar/falcon/navigator/MainActivity.kt
+++ app/src/main/java/xyz/javidsattar/falcon/navigator/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.javidsattar.falcon.navigator.ui.joystick.JoystickScreen
 import xyz.javidsattar.falcon.navigator.ui.theme.FalconNavigatorTheme
@@ -18,11 +20,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            LaunchedEffect(true) {
+                updateStatusBarIcons()
+            }
             FalconNavigatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     JoystickScreen(Modifier.padding(innerPadding))
                 }
             }
         }
+    }
+    private fun updateStatusBarIcons() {
+        val window = window
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.isAppearanceLightStatusBars = false
     }
 }
