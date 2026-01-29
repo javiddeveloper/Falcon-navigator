528cf262faf2482d79f870acdd868e5d4d8cc38a
javid <javiddeveloper@gmail,com>
2026-01-29 15:32:50 +0330

    feat: Enable Hilt for dependency injection
    Add the `@AndroidEntryPoint` annotation to `MainActivity` to enable dependency injection using Hilt.

--- app/src/main/java/xyz/javidsattar/falcon/navigator/MainActivity.kt
+++ app/src/main/java/xyz/javidsattar/falcon/navigator/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import xyz.javidsattar.falcon.navigator.ui.joystick.JoystickScreen
 import xyz.javidsattar.falcon.navigator.ui.theme.FalconNavigatorTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
